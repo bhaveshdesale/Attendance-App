2793186d7eb6d524782fd0c68a98dbfc640c828a
Bhavesh <143746617+bhaveshdesale@users.noreply.github.com>
2024-10-19 09:19:26 +0530

    This is 2nd commit from my side

--- app/src/main/java/com/android/attendance/activity/MainActivity.java
+++ app/src/main/java/com/android/attendance/activity/MainActivity.java
@@ -24,7 +24,7 @@ public class MainActivity extends Activity {
 			public void onClick(View v) {
 				// TODO Auto-generated method stub
 
-				Intent intent =new Intent(MainActivity.this,AddFacultyActivity.class);
+				Intent intent =new Intent(MainActivity.this,LoginActivity.class);
 				startActivity(intent);
 			}
 		});
